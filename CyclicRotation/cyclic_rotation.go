c67175e59d311307331df90deaf4257f109b0ff9
Richie Varghese <ric-v@outlook.com>
2020-12-09 22:55:15 +0530

    updated rotations comment

--- CyclicRotation/cyclic_rotation.go
+++ CyclicRotation/cyclic_rotation.go
@@ -3,7 +3,10 @@
 // Author : Richie Varghese
 // Date   : 08/12/2020
 //
-// Cyclic Rotation : An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+// Cyclic Rotation : An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index,
+//                   and the last element of the array is moved to the first place.
+//                   For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]
+//                   (elements are shifted right by one index and 6 is moved to the first place).
 //
 // Functions : cyclicRotation ---> rotates the input array to right by n positions
 //
